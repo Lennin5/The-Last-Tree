5b7b7a45701e3f160f2dc864fe78f22187197252
Lennin5 <lenninlemus0@gmail.com>
2024-03-14 10:05:06 -0600

    feat(nivel harmony groove): agregado de nuevos asstes de hojas con diferente perspectiva y script de animación de hojas cayendo y desapareciendo

--- Scripts/FadeOut.cs
+++ Scripts/FadeOut.cs
@@ -4,8 +4,9 @@ using UnityEngine.UI;
 
 public class FadeOut : MonoBehaviour
 {
-    public float fadeDuration = 2f; // Duración de la desaparición gradual
+    public float fadeDuration = 0.2f; // Duración de la desaparición gradual
     public Image image;
+    public Rigidbody2D rb;
 
     void Start()
     {
@@ -16,18 +17,29 @@ public class FadeOut : MonoBehaviour
             return;
         }
 
-        // Inicia la desaparición gradual
-        StartCoroutine(FadeOutCoroutine());
-
-        // Inicia la desaparición gradual con un retraso de 2 segundos
+        // Inicia la desaparición gradual con un lapso de tiempo aleatorio
+        StartCoroutine(StartFadeOutWithDelay());
 
     }
+
+    IEnumerator StartFadeOutWithDelay()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 5f));
+        StartCoroutine(FadeOutCoroutine());
+    }
+
 
     IEnumerator FadeOutCoroutine()
     {
         float elapsedTime = 0f;
+        // almacenar la posición inicial
+        int initialX = (int)image.rectTransform.position.x;
+        int initialY = (int)image.rectTransform.position.y;
         Color initialColor = image.color;
         Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 0f); // Alfa = 0
+
+        rb.gravityScale = 10f;
+
 
         while (elapsedTime < fadeDuration)
         {
@@ -41,6 +53,27 @@ public class FadeOut : MonoBehaviour
         image.color = targetColor;
 
         // Desactiva el objeto después de desaparecer completamente (opcional)
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
+
+        // Reiniciar la opacidad
+        image.color = initialColor;
+
+        // obtener un initialXRandom en base de un rango de valores basado en la resolución de la pantalla en X
+        int initialXRandom = Random.Range(0, Screen.width);
+
+        // Reiniciar la posición de la imagen
+        image.rectTransform.position = new Vector3(initialXRandom, initialY, image.rectTransform.position.z);
+
+        // Reiniciar la gravedad
+        rb.gravityScale = 0f;
+
+        // Detener el movimiento del objeto
+        rb.velocity = Vector2.zero;
+
+        // Volver a iniciar la desaparición gradual en un lapso de tiempo aleatorio
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.9f));
+        StartCoroutine(FadeOutCoroutine());
+        
     }
+
 }
